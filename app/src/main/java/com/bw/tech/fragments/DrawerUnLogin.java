89ae40af66f540183d9a12e590b270c5b309bd30
贾继飞 <771420655@qq.com>
2020-11-14 09:17:00 +0800

    未登录状态下的侧滑框点击跳转到登录页面

--- app/src/main/java/com/bw/tech/fragments/DrawerUnLogin.java
+++ app/src/main/java/com/bw/tech/fragments/DrawerUnLogin.java
@@ -1,17 +1,32 @@
 package com.bw.tech.fragments;
 
+import android.content.Intent;
+import android.view.View;
+import android.widget.LinearLayout;
+
 import com.bw.mylibrary.base.BaseFragment;
+import com.bw.tech.MyApp;
 import com.bw.tech.R;
+import com.bw.tech.activities.LoginActivity;
 
 public class DrawerUnLogin extends BaseFragment {
+    private LinearLayout log_out_layout;
     @Override
     public void initView() {
-
+        View view=getView();
+        log_out_layout=view.findViewById(R.id.log_out_layout);
     }
 
     @Override
     public void initData() {
-
+        //点击跳转到登录页面
+        log_out_layout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(getActivity(), LoginActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
     @Override
